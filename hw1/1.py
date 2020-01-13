1e808f4bdb6a9fe4d40fadc20717a465d86266e7
joebobs0n <czech.monk90@gmail.com>
2020-01-13 13:17:08 -0700

    initial commit

---
+++ hw1/1.py
@@ -0,0 +1,32 @@
+import cv2 as cv
+import numpy as np
+
+target_width = 800
+target_height = 600
+
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+	print("Cannot open camera")
+	exit()
+
+if not (target_width == cap.get(cv.CAP_PROP_FRAME_WIDTH)):
+	cap.set(cv.CAP_PROP_FRAME_WIDTH, target_width)
+if not (target_height == cap.get(cv.CAP_PROP_FRAME_HEIGHT)):
+	cap.set(cv.CAP_PROP_FRAME_HEIGHT, target_height)
+
+if not (target_width == cap.get(cv.CAP_PROP_FRAME_WIDTH)):
+	print("Width wrong and unset")
+if not (target_height == cap.get(cv.CAP_PROP_FRAME_HEIGHT)):
+	print("Height wrong and unset")
+
+ret, frame = cap.read()
+if not ret:
+	print("Can't receive frame (stream end?). Exiting...")
+	exit()
+# gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+cv.imshow('Monk',frame)
+cv.imwrite('1.jpg',frame)
+
+cv.waitKey(0)
+cap.release()
+cv.destroyAllWindows()
---
+++ video_feed.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+import numpy as np
+
+print("Hit ESC to exit feed")
+
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+	print("Cannot open camera")
+	exit()
+
+while True:
+	ret, frame = cap.read()
+	if not ret:
+		print("Can't receive frame (stream end?). Exiting...")
+		break
+	cv.imshow('Monk',frame)
+	if cv.waitKey(1) == 27:
+		break
+
+cap.release()
+cv.destroyAllWindows()
