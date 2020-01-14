eea518e57ef77a01caeb5c6d1ed999b0d358cdba
joebobs0n <czech.monk90@gmail.com>
2020-01-13 19:32:01 -0700

    fully functional and commented

--- hw1/2.py
+++ hw1/2.py
@@ -2,92 +2,88 @@ import imutils
 import cv2 as cv
 import numpy as np
 
-tgt_fps = 60
-tgt_height = 600
+tgt_fps = 60 # desired camera fps - doesn't seem to work on pc, but does on raspberry pi
+tgt_height = 600 # change height of processed/displayed video - width changed proportionally
 
-print("\nUsage Instructions:\n -ESC: exit feed\n -0: original mode\n -1: binarize mode\n -2: canny edges mode\n -3: "
-      "corner detect mode\n -4: line detect mode\n -5: difference mode\n")
+# print out instructions to console
+print("\nUsage Instructions:\n -ESC: exit feed\n -0: original mode\n -1: binarize mode\n -2: canny edges mode\n -3: corner detect mode\n -4: line detect mode\n -5: difference mode\n")
 
-mode = 0
-cap = cv.VideoCapture(0)
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
-first_flag = True
-cap.set(cv.CAP_PROP_FPS, tgt_fps)
-print("Starting OpenCV program at {} FPS".format(cap.get(cv.CAP_PROP_FPS)))
+mode = 0 # initialize current mode variable
+cap = cv.VideoCapture(0) # initialize video capture
+if not cap.isOpened(): # check if camera successfully opened
+    print("Cannot open camera") # if not - inform user
+    exit() # exit program
+cap.set(cv.CAP_PROP_FPS, tgt_fps) # set target fps
+print("Starting OpenCV program at {} FPS".format(cap.get(cv.CAP_PROP_FPS))) # inform user of actual fps
 
-while True:
-    ret, frame = cap.read()
+while True: # run continuously
+    ret, frame = cap.read() # read frame from camera capture
+    if not ret: # if no frame received
+        print("Can't receive frame. Exiting...") # inform user
+        break # break from unconditional loop and exit program
+    frame = imutils.resize(frame, height=tgt_height) # resize received frame to target height
 
-    if not ret:
-        print("Can't receive frame. Exiting...")
-        break
-
-    frame = imutils.resize(frame, height=tgt_height)
-
-    k = cv.waitKey(1) & 0xFF
-    if k == 27:
-        break
+    k = cv.waitKey(1) & 0xFF # wait 1 ms for keypress
+    if k == 27: # if 'esc' received
+        break # break from unconditional loop and exit program
     elif k == ord('`'):
-        mode = 0
+        mode = 0 # change to original video mode (no processing)
     elif k == ord('1'):
-        mode = 1
+        mode = 1 # change to threshold mode
     elif k == ord('2'):
-        mode = 2
+        mode = 2 # change to edge detection mode
     elif k == ord('3'):
-        mode = 3
+        mode = 3 # change to corner detection mode
     elif k == ord('4'):
-        mode = 4
+        mode = 4 # change to line detection mode
     elif k == ord('5'):
-        mode = 5
+        mode = 5 # change to frame differencing mode
     else:
-        pass
+        pass # ignore all other keystrokes
+    print("\rMode {} selected".format(mode), end="") # inform user of currently selected mode
 
-    print("\rMode {} selected".format(mode), end="")
-
-    disp_frame = []
+    disp_frame = [] # initialize display frame (actual frame passed to user viewer)
     if mode == 0:
         # original
-        disp_frame = frame
+        disp_frame = frame # pass received frame to user viewer
     elif mode == 1:
         # binarize/threshold
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        disp_frame = cv.threshold(gray, 125, 250, cv.THRESH_BINARY_INV)[1]
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) # convert frame to grayscale
+        disp_frame = cv.threshold(gray, 125, 250, cv.THRESH_BINARY_INV)[1] # convert grayscale to black and white
     elif mode == 2:
         # canny edge detection
-        disp_frame = cv.Canny(frame, 30, 150)
+        disp_frame = cv.Canny(frame, 30, 150) # convert frame to detected edges
     elif mode == 3:
         # corner detection
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        gray = np.float32(gray)
-        dst = cv.cornerHarris(gray, 2, 3, 0.04)
-        dst = cv.dilate(dst, None)
-        ret, dst = cv.threshold(dst, 0.01 * dst.max(), 255, 0)
-        dst = np.uint8(dst)
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) # convert frame to grayscale
+        gray = np.float32(gray) # convert frame array to float32 for computation
+        dst = cv.cornerHarris(gray, 2, 3, 0.04) # detect corners
+        dst = cv.dilate(dst, None) # dilate detected corners
+        ret, dst = cv.threshold(dst, 0.01 * dst.max(), 255, 0) # convert corner data to black and white
+        dst = np.uint8(dst) # convert corner data to unsigned 8b ints for computation
 
-        ret, labels, stats, centroids = cv.connectedComponentsWithStats(dst)
+        ret, labels, stats, centroids = cv.connectedComponentsWithStats(dst) # don't know :|
 
-        criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 100, 0.001)
-        corners = cv.cornerSubPix(gray, np.float32(centroids), (5, 5), (-1, -1), criteria)
+        criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 100, 0.001) # create object of 'criteria' data
+        corners = cv.cornerSubPix(gray, np.float32(centroids), (5, 5), (-1, -1), criteria) # find sub pixel corners
 
-        res = np.hstack((centroids, corners))
-        res = np.int0(res)
-        disp_frame = frame
-        # disp_frame[res[:, 1], res[:, 0]] = [0, 0, 255]
-        if res is not None:
-            for point in res[1:len(res)]:
-                cv.circle(disp_frame, (point[0], point[1]), 2, (0, 0, 255), 2)
+        res = np.hstack((centroids, corners)) # don't know :/
+        res = np.int0(res) # don't know :(
+        disp_frame = frame # pass received frame to user viewer frame
+        if res is not None: # if there are corners detected
+            for point in res[1:len(res)]: # work through each point
+                cv.circle(disp_frame, (point[0], point[1]), 2, (0, 0, 255), 2) # draw a circle at the given point
     elif mode == 4:
         # line detection
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        edges = cv.Canny(gray, 50, 150, apertureSize=3)
-        lines = cv.HoughLines(edges, 1, np.pi / 180, 200)
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) # convert frame to grayscale
+        edges = cv.Canny(gray, 50, 150, apertureSize=3) # detect edges
+        lines = cv.HoughLines(edges, 1, np.pi / 180, 200)n # detect lines from edges data
 
-        disp_frame = frame
-        if lines is not None:
-            for oneLine in lines:
-                for r, theta in oneLine:
+        disp_frame = frame # pass received frame to user viewer frame
+        if lines is not None: # if there are lines detected
+            for oneLine in lines: # work through each line
+                for r, theta in oneLine: # work through each radius and angle for each line
+                    # calculate two x and y coordinates for each line
                     a = np.cos(theta)
                     b = np.sin(theta)
                     x0 = a * r
@@ -96,19 +92,21 @@ while True:
                     y1 = int(y0 + 1000 * (a))
                     x2 = int(x0 - 1000 * (-b))
                     y2 = int(y0 - 1000 * (a))
+                    # draw a line corresponding to each set of coordinates
                     cv.line(disp_frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
     elif mode == 5:
         # difference mode
-        frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        ret, frame2 = cap.read()
-        frame2 = imutils.resize(frame2, height=tgt_height)
-        frame2 = cv.cvtColor(frame2, cv.COLOR_BGR2GRAY)
-        disp_frame = cv.absdiff(frame2, frame)
+        frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) # convert frame data to grayscale
+        ret, frame2 = cap.read() # receive another frame from camera
+        frame2 = imutils.resize(frame2, height=tgt_height) # resize second frame
+        frame2 = cv.cvtColor(frame2, cv.COLOR_BGR2GRAY) # convert second frame to grayscale
+        disp_frame = cv.absdiff(frame2, frame) # use both frames to find their absolute difference
     else:
-        pass
+        mode = 0 # invalid mode encountered - change back to mode 0 (original capture)
 
-    cv.imshow("monk-hw1_p2", disp_frame)
-    disp_frame = None
+    cv.imshow("monk-hw1_p2", disp_frame) # display processed frame
+    disp_frame = None # empty frame data
 
-cap.release()
-cv.destroyAllWindows()
+# while loop broken
+cap.release() # release camera
+cv.destroyAllWindows() # close all windows generated by opencv
